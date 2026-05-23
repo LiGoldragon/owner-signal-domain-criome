98c674f2556341f796ec1bf639d4431cd086486d
li <li@goldragon.criome.net>
2026-05-23 20:32:20 +0200

    owner-signal-domain-criome: drop rejection operation echo

--- src/lib.rs
+++ src/lib.rs
@@ -107,7 +107,6 @@ pub enum RejectionReason {
 
 #[derive(Archive, RkyvSerialize, RkyvDeserialize, NotaRecord, Debug, Clone, PartialEq, Eq)]
 pub struct RequestRejected {
-    pub operation: OperationKind,
     pub reason: RejectionReason,
 }
 
